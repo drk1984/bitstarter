f830b39ae21976c89861870ecbde3166eee03eab
drk1984 <darshankadia@gmail.com>
2014-01-19 17:21:15 -0800

    initial release

---
+++ web.js
@@ -0,0 +1,12 @@
+// Node js server 
+var express = require('express');
+var app = express.createServer(express.logger());
+var fs = require('fs');
+var port = process.env.PORT || 5000
+
+app.get('/',function(request,response) { response.send('Wassup?!');});
+
+app.listen(port, function() { console.log('Listening on '+port);});
+
+
+
