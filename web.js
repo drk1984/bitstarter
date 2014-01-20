41c0c46f07c0aa9a4ff7a3bb816de2975769836c
drk1984 <darshankadia@gmail.com>
2014-01-19 18:28:30 -0800

    deploy msg edited

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var port = process.env.PORT || 5000
 
-var indextext = fs.readFileSync('index.html').toString();
-app.get('/',function(request,response) { response.send(indextext);});
+var indextext = fs.readFileSync('index.html').toString().split('\n');
+app.get('/',function(request,response) { response.send(indextext[0]+'\n'+indextext[1]);});
 
 app.listen(port, function() { console.log('Listening on '+port);});
 
