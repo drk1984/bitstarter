2783075aea9a0e854b4d6874e49277cdbbc43789
drk1984 <darshankadia@gmail.com>
2014-01-19 18:12:47 -0800

    changed deploy msg

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var port = process.env.PORT || 5000
 
-app.get('/',function(request,response) { response.send('Wassup?!');});
+app.get('/',function(request,response) { response.send('Wassup ?! :-) ');});
 
 app.listen(port, function() { console.log('Listening on '+port);});
 
